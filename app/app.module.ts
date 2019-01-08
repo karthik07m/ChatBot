f65d139c0e78b9c2cbb625bdea882ba90fa5e729
Mani Karthik <karthik07m@gmail.com>
2019-01-08 08:22:23 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import {ChatModule} from './chat/chat.module'
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ChatModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
